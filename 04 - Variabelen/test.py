17fbc426ef53a425d78f00f99e6543d8f951b73f
simonvandevannet <43027860+simonvandevannet@users.noreply.github.com>
2018-09-27 09:35:04 +0200

    oppervlakte cirkel

---
+++ 04 - Variabelen/test.py
@@ -0,0 +1,8 @@
+aantal_leerlingen_in_klas = 18
+
+nieuwe_leerlingen = int(input('Aantal nieuwe leerlingen: '))
+
+# aantal_leerlingen_in_klas = aantal_leerlingen_in_klas + nieuwe_leerlingen
+aantal_leerlingen_in_klas *= nieuwe_leerlingen
+
+print(aantal_leerlingen_in_klas)

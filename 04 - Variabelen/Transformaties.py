42b146bf20489952e4f1536ffc2de69736cf9313
simonvandevannet <43027860+simonvandevannet@users.noreply.github.com>
2018-09-29 11:04:10 +0200

    transformaties

---
+++ 04 - Variabelen/Transformaties.py
@@ -0,0 +1,11 @@
+fx = str('f(x) = 2(x - 3)^2 + 4')
+
+#input
+a = float(input('Geef a: '))
+b = float(input('Geef b: '))
+
+#output
+gx = str('g(x) = 2(x - ') + str(3 + a) + str(')^2 + ') + str(4 + b)
+
+print(fx)
+print(gx)

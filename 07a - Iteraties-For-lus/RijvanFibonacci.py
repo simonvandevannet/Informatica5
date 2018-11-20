d1bdee0bf6bc4ca0c84e34f289ee8265f3af6064
simonvandevannet <43027860+simonvandevannet@users.noreply.github.com>
2018-11-20 15:35:09 +0100

    Rij van Fibonacci

--- 07a - Iteraties-For-lus/RijvanFibonacci.py
+++ 07a - Iteraties-For-lus/RijvanFibonacci.py
@@ -11,3 +11,17 @@ for _ in range(n - 1):
 
 # uitvoer
 print(uitvoer)
+
+
+
+# oplossing
+n = int(input('hoeveelste getal van Fibonacci'))
+
+vorige = 1
+huidige = 1
+# vorige, huidige = 1,1
+
+for i in range(n - 2):
+    vorige, huidige = huidige, huidige + vorige
+
+print(huidige)

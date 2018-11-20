e8f68de7dc72cf0f828e0cc7b2ac5beb08a08291
simonvandevannet <43027860+simonvandevannet@users.noreply.github.com>
2018-11-20 15:34:45 +0100

    Het grootste en het gemiddelde

--- 07a - Iteraties-For-lus/HetGrootsteenhetGemiddelde.py
+++ 07a - Iteraties-For-lus/HetGrootsteenhetGemiddelde.py
@@ -14,3 +14,21 @@ gemiddelde = som / aantal_getallen
 
 # uitvoer
 print('Het grootste getal is {:.0f} en het gemiddelde is {:.2f}'.format(max, gemiddelde))
+
+
+
+# oplossing
+aantal = int(input('aantal'))
+
+grootste = int(input('getal: '))
+som = grootste
+
+for i in range(aantal - 1):
+    getal = int(input('getal: '))
+    som += getal
+    if(getal > grootste):
+        grootste = getal
+
+print('Het grootste getal is {} en het gemiddelde is {:.2f}'.format(grootste, som / aantal))
+
+# bij grootste of kleinste steeds een getal voor de lus inlezen

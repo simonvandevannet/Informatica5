065d73195cbfc0c98c8205691da4d93e9053a015
simonvandevannet <43027860+simonvandevannet@users.noreply.github.com>
2018-11-28 18:01:02 +0100

    Vermoeden van Collatz

---
+++ 08 - Functies/Vermoeden_van_Collatz.py
@@ -0,0 +1,11 @@
+def volgend_collatz_getal(n):
+    if n % 2 == 0:
+        volgend = n // 2
+    elif n % 2 == 1:
+        volgend = n * 3 + 1
+    return volgend
+
+def collatz(n):
+    aantal
+    while volgend != 1:
+        aantal += 1

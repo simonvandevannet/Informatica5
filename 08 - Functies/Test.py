b1503e6b607012a0ff7fec7dd912c063b3091e68
simonvandevannet <43027860+simonvandevannet@users.noreply.github.com>
2019-01-06 12:30:00 +0100

    Vermoeden van Collatz

---
+++ 08 - Functies/Test.py
@@ -0,0 +1,9 @@
+serienummer = int(input('serienummer: '))
+aantal = 1
+
+while serienummer >= 0:
+    serienummer = int(input('serienummer: ')
+    aantal += 1
+
+print(aantal)
+
--- 08 - Functies/Vermoeden_van_Collatz.py
+++ 08 - Functies/Vermoeden_van_Collatz.py
@@ -6,6 +6,13 @@ def volgend_collatz_getal(n):
     return volgend
 
 def collatz(n):
-    aantal
-    while volgend != 1:
+    aantal = 1
+    volgend = volgend_collatz_getal(n)
+    if n == 1:
+        aantal = 1
+    else:
+        while volgend != 1:
+            aantal += 1
+            volgend = volgend_collatz_getal(volgend)
         aantal += 1
+    return aantal

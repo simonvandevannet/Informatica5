0cbb2f2fbd7983a8616a96e03008822fe5f91e5a
simonvandevannet <43027860+simonvandevannet@users.noreply.github.com>
2019-01-17 09:27:01 +0100

    Palindroom

--- 10 - Strings/Palindroom.py
+++ 10 - Strings/Palindroom.py
@@ -7,3 +7,15 @@ def is_palindroom(woord):
     else:
         mes = False
     return mes
+
+def palindroom(woord):
+
+    i = 0
+
+    while woord[i]  == woord[-i - 1] and i < len(woord) // 2:
+        i += 1
+
+    return i == len(woord) // 2
+
+print(palindroom('lepel'))
+

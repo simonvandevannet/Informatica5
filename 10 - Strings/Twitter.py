b847df67485067c4ea7878fe2f833c7838c991ef
simonvandevannet <43027860+simonvandevannet@users.noreply.github.com>
2019-01-17 09:27:21 +0100

    Twitter

--- 10 - Strings/Twitter.py
+++ 10 - Strings/Twitter.py
@@ -10,3 +10,19 @@ for i in range(0, len(zin)):
         hashtag += zin[plaatsh + 1: plaatss] + '\n'
 
 print(hashtag)
+
+tweet = input('geef tweet: ')
+
+i_hashtag = tweet.find('#')
+
+while i_hashtag != -1:
+
+    # tweet afknippen net na het #-teken
+    tweet = tweet[i_hashtag + 1:]
+    i_spatie = tweet.find(' ')
+
+    # hashtag uitknippen (en printen)
+    print(tweet[:i_spatie])
+
+    # terug op zoek naar een #
+    i_hashtag = tweet.find('#')
